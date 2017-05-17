96baf293c3007a6fd39de4384b203dce09123ef9
Rfeng <rfeng83@gmail.com>
2017-05-17 15:34:53 +0800

    add field

--- CarfixIphone/Services/MobileUserResponses/GetOfferServicesResponse.swift
+++ CarfixIphone/Services/MobileUserResponses/GetOfferServicesResponse.swift
@@ -16,6 +16,7 @@ class GetOfferServicesResponse: MobileUserAPIResponse {
 class GetOfferServicesResult: BaseAPIItem {
     var ServiceID: Int = 0
     var ServiceName: String?
+    var IsSubscribed: NSNumber?
     var InsurerName: String?
     var Title: String?
     var SubTitle: String?
